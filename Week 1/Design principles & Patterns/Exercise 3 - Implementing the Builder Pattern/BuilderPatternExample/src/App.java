6d2ad0bcd8b93ead50153b676a902e34a71606d1
Sharvesh <sharveshguru2004@gmail.com>
2025-06-21 18:05:51 +0530

    Implement driver code

--- Week 1/Design principles & Patterns/Exercise 3 - Implementing the Builder Pattern/BuilderPatternExample/src/App.java
+++ Week 1/Design principles & Patterns/Exercise 3 - Implementing the Builder Pattern/BuilderPatternExample/src/App.java
@@ -1,5 +1,19 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Computer basicComputer = new Computer.Builder("Intel i5", "8GB", "256GB SSD")
+                .build();
+        
+        Computer gamingComputer = new Computer.Builder("AMD Ryzen 7", "16GB", "1TB SSD")
+                .setCPU("AMD Ryzen 9") 
+                .setRAM("32GB") 
+                .build();
+
+        Computer workstationComputer = new Computer.Builder("Intel Xeon", "64GB", "2TB SSD")
+                .setStorage("4TB SSD") 
+                .build();
+        
+        System.out.println(basicComputer);
+        System.out.println(gamingComputer);
+        System.out.println(workstationComputer);
     }
 }
