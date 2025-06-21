53fc6bced64bb929f7700061c0b4bbc1a2357a7c
Sharvesh <sharveshguru2004@gmail.com>
2025-06-21 16:59:47 +0530

    modifications

--- Week 1/Data structures and Algorithms/Exercise 7 - Financial Forecasting/FinancialForecastUsingRecursion/src/App.java
+++ Week 1/Data structures and Algorithms/Exercise 7 - Financial Forecasting/FinancialForecastUsingRecursion/src/App.java
@@ -1,5 +1,18 @@
+import java.util.HashMap;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        double presentValue = 6700.0; 
+        double interestRate = 0.17; 
+        int years = 10; 
+
+        double futureValue = FinancialForecast.calculateFutureValue(presentValue, interestRate, years);
+        System.out.println("Future Value Calculation using basic Recursion:");
+        System.out.printf("Future Value after %d years: %.2f%n", years, futureValue);
+
+        HashMap <Integer, Double> memo = new HashMap<>();
+        futureValue = FinancialForecast.memoizedCalculateFutureValue(presentValue, interestRate, years, memo);
+        System.out.println("\nFuture Value Calculation using Recursion and Memoization:");
+        System.out.printf("Future Value after %d years: %.2f%n", years, futureValue);
     }
 }
