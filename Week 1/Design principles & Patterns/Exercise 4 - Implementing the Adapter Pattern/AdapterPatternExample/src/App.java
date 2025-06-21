38487157515b578577ec68e7d5c1361b93fcdbae
Sharvesh <sharveshguru2004@gmail.com>
2025-06-21 18:41:42 +0530

    Implement driver code

--- Week 1/Design principles & Patterns/Exercise 4 - Implementing the Adapter Pattern/AdapterPatternExample/src/App.java
+++ Week 1/Design principles & Patterns/Exercise 4 - Implementing the Adapter Pattern/AdapterPatternExample/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        PayPalGateway paypal = new PayPalGateway();
+        PaymentProcessor paypalProcessor = new PayPalAdapter(paypal);
+        paypalProcessor.processPayment(150.00);
+
+        RazorpayGateway razorpay = new RazorpayGateway();
+        PaymentProcessor razorpayProcessor = new RazorpayAdapter(razorpay); 
+        razorpayProcessor.processPayment(200.00);
     }
 }
