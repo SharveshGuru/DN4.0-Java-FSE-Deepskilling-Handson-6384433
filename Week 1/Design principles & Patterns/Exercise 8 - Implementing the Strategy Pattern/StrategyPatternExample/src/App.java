f938dbd29801436625f01acf0f3a31dc515180bf
Sharvesh <sharveshguru2004@gmail.com>
2025-06-22 12:14:45 +0530

    update output

--- Week 1/Design principles & Patterns/Exercise 8 - Implementing the Strategy Pattern/StrategyPatternExample/src/App.java
+++ Week 1/Design principles & Patterns/Exercise 8 - Implementing the Strategy Pattern/StrategyPatternExample/src/App.java
@@ -1,5 +1,11 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        PaymentContext paymentContext = new PaymentContext();
+        
+        paymentContext.setPaymentStrategy(new PayPalPayment("31231saff"));
+        paymentContext.pay(100.0);
+
+        paymentContext.setPaymentStrategy(new CreditCardPayment("1234-5678-9876-5432"));
+        paymentContext.pay(200.0);
     }
 }
