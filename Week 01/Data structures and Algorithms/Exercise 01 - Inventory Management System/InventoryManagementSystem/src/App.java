5d53448431ba98badf044d9d564f6ea8e04a503c
Sharvesh <sharveshguru2004@gmail.com>
2025-06-22 14:14:02 +0530

    update driver code

--- Week 01/Data structures and Algorithms/Exercise 01 - Inventory Management System/InventoryManagementSystem/src/App.java
+++ Week 01/Data structures and Algorithms/Exercise 01 - Inventory Management System/InventoryManagementSystem/src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        InventoryManager manager = new InventoryManager();
+
+        Product p1 = new Product(101, "Laptop", 50, 70000);
+        Product p2 = new Product(102, "Mouse", 200, 500);
+        
+        manager.addProduct(p1);
+        manager.addProduct(p2);
+
+        manager.viewAllProducts();
+
+        manager.updateProduct(101, "Gaming Laptop", 40, 85000);
+        manager.viewAllProducts();
+
+        manager.deleteProduct(102);
+        manager.viewAllProducts();
+
     }
 }
