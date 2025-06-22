c56723b87cae952f667b6dcf0481820037e37247
Sharvesh <sharveshguru2004@gmail.com>
2025-06-22 12:52:00 +0530

    tested dependency injection

--- Week 1/Design principles & Patterns/Exercise 11 - Implementing Dependency Injection/DependencyInjectionExample/src/App.java
+++ Week 1/Design principles & Patterns/Exercise 11 - Implementing Dependency Injection/DependencyInjectionExample/src/App.java
@@ -1,5 +1,8 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        CustomerRepository customerRepository = new CustomerRepositoryImpl();
+        CustomerService customerService = new CustomerService(customerRepository);
+
+        customerService.getCustomerDetails(150);
     }
 }
