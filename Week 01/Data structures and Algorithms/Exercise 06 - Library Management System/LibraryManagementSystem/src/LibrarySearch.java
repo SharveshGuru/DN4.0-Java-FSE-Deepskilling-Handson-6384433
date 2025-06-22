92677587b44ef63bddd3d7cf412c3372a9168f41
Sharvesh <sharveshguru2004@gmail.com>
2025-06-22 18:59:00 +0530

    implemented linear search

---
+++ Week 01/Data structures and Algorithms/Exercise 06 - Library Management System/LibraryManagementSystem/src/LibrarySearch.java
@@ -0,0 +1,10 @@
+public class LibrarySearch {
+    public static Book linearSearchByTitle(Book[] books, String title) {
+        for (Book book : books) {
+            if (book.title.equalsIgnoreCase(title)) {
+                return book;
+            }
+        }
+        return null;
+    }
+}
