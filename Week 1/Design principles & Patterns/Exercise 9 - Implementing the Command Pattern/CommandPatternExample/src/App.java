066890179d65db207289f9dde681cfb9d30ec59e
Sharvesh <sharveshguru2004@gmail.com>
2025-06-22 12:25:57 +0530

    implement driver code

--- Week 1/Design principles & Patterns/Exercise 9 - Implementing the Command Pattern/CommandPatternExample/src/App.java
+++ Week 1/Design principles & Patterns/Exercise 9 - Implementing the Command Pattern/CommandPatternExample/src/App.java
@@ -1,5 +1,14 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Light light = new Light();
+
+        Command lightOn = new LightOnCommand(light);
+        Command lightOff = new LightOffCommand(light);
+
+        RemoteControl remote = new RemoteControl();
+        remote.setCommand(lightOn);
+        remote.pressButton(); 
+        remote.setCommand(lightOff);
+        remote.pressButton();
     }
 }
