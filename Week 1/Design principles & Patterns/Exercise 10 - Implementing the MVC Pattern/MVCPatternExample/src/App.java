2205736760e4c534ee850dda49dd1c07b7ed3f6b
Sharvesh <sharveshguru2004@gmail.com>
2025-06-22 12:40:34 +0530

    implemented driver code

--- Week 1/Design principles & Patterns/Exercise 10 - Implementing the MVC Pattern/MVCPatternExample/src/App.java
+++ Week 1/Design principles & Patterns/Exercise 10 - Implementing the MVC Pattern/MVCPatternExample/src/App.java
@@ -1,5 +1,14 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        Student student = new Student("John Doe", "12345", "A");
+        StudentView view = new StudentView();
+        StudentController controller = new StudentController(student, view);
+        
+        controller.updateView();
+
+        controller.setStudentName("Ken Adams");
+        controller.setStudentId("54321");   
+        controller.setStudentGrade("B+");
+        controller.updateView();
     }
 }
