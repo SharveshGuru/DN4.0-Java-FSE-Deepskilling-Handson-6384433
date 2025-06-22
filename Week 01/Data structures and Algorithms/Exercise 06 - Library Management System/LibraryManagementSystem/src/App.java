e2f8047dc94f39a577624ee8d84ab787a7870ea2
Sharvesh <sharveshguru2004@gmail.com>
2025-06-22 19:02:13 +0530

    implemented driver code

--- Week 01/Data structures and Algorithms/Exercise 06 - Library Management System/LibraryManagementSystem/src/App.java
+++ Week 01/Data structures and Algorithms/Exercise 06 - Library Management System/LibraryManagementSystem/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Book[] books = {
+            new Book(1, "Harry Potter and The Philosopher's Stone", "J.K. Rowling"),
+            new Book(2, "1984", "George Orwell"),
+            new Book(3, "The Martian", "Andy Weir"),
+            new Book(4, "Crime and Punishment", "Fyodor Dostoevsky"),
+        };
+
+        Book result1 = LibrarySearch.linearSearchByTitle(books, "1984");
+        System.out.println("Linear Search Result: " + result1);
+
+        Book result2 = LibrarySearch.binarySearchByTitle(books, "Crime and Punishment");
+        System.out.println("Binary Search Result: " + result2);
     }
 }
