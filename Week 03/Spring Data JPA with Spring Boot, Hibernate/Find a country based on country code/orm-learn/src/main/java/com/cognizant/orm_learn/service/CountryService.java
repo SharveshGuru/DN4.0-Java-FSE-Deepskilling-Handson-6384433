567b5a5590d79a7dbedc0e742971c495e4e3f07e
Sharvesh <sharveshguru2004@gmail.com>
2025-07-05 23:49:25 +0530

    hands on completed

---
+++ Week 03/Spring Data JPA with Spring Boot, Hibernate/Find a country based on country code/orm-learn/src/main/java/com/cognizant/orm_learn/service/CountryService.java
@@ -0,0 +1,32 @@
+package com.cognizant.orm_learn.service;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.cognizant.orm_learn.model.Country;
+import com.cognizant.orm_learn.repository.CountryRepository;
+import com.cognizant.orm_learn.service.exception.CountryNotFoundException;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+
+    @Transactional
+    public Country findCountryByCode(String countryCode) throws CountryNotFoundException {
+        Optional<Country> result = countryRepository.findById(countryCode);
+        if (!result.isPresent()) {
+            throw new CountryNotFoundException("Country not found with code: " + countryCode);
+        }
+        return result.get();
+    }
+}
+
+
